a91e42fc5e282c7e6582b23f53b84707c03807f8
jktravis <joshua.travis@gmail.com>
2015-12-13 18:34:54 -0500

    Formatting changes.

--- static/js/script.js
+++ static/js/script.js
@@ -1,91 +1,89 @@
-(function(){
-   $(document).ready(function()
-   {
-     $.ajax({
-       url: 'static/js/calculator.js',
-       dataType: "script",
-       success: function (data, status, jqxhr)
-       {
-         var calc = exports.calculator;
-         var $display = $('.output').first();
-         calc.total = 0;
-         calc.input = '';
-         calc.display = 0;
-         var op = false;
-
-         updateDisplay(calc.total);
-
-      $('button').on('click', function()
+(function ()
+{
+  $(document).ready(function ()
+  {
+    $.ajax({
+      url: 'static/js/calculator.js',
+      dataType: "script",
+      success: function (data, status, jqxhr)
       {
-         var currentButton = $(this).val();
-        // if the button is a number or a period
-        if (!isNaN(parseInt(currentButton)) || currentButton === '.')
-         {
+        var calc = exports.calculator;
+        var $display = $('.output').first();
+        calc.total = 0;
+        calc.display = 0;
+
+        updateDisplay(calc.total);
+
+        $('button').on('click', function ()
+        {
+          var currentButton = $(this).val();
+          // if the button is a number or a period
+          if (!isNaN(parseInt(currentButton)) || currentButton === '.')
+          {
             //store value for math operation
             if (calc.display)
-           {
-               calc.display += '' + currentButton;
+            {
+              calc.display += '' + currentButton;
             }
             else
             {
-               calc.display = currentButton;
+              calc.display = currentButton;
             }
-           calc.input = calc.display;
-           updateDisplay(calc.display);
-         }
-         else
-         {
+            updateDisplay(calc.display);
+          }
+          else
+          {
             // store operation to perform function
-           switch (currentButton)
-           {
-             case 'AC':
-               allClear();
-               break;
-             case 'CE':
-               clearCurrent();
-               break;
-             case '/':
-               console.log('Dividing');
-               break;
-             case '*':
-               console.log('Multiplying');
-               break;
-             case '%':
-               console.log('Percenting');
-               break;
-             case '+':
-               console.log('Adding');
-               break;
-             case '-':
-               console.log('Subtracting');
-               break;
-             case '=':
-               console.log('Equals');
-               break;
-             default:
-               break;
-           }
-         }
-      });
+            switch (currentButton)
+            {
+              case 'AC':
+                allClear();
+                break;
+              case 'CE':
+                clearCurrent();
+                break;
+              case '/':
+                console.log('Dividing');
+                break;
+              case '*':
+                console.log('Multiplying');
+                break;
+              case '%':
+                console.log('Percenting');
+                break;
+              case '+':
+                console.log('Adding');
+                break;
+              case '-':
+                console.log('Subtracting');
+                break;
+              case '=':
+                console.log('Equals');
+                break;
+              default:
+                break;
+            }
+          }
+        });
 
-         function clearCurrent()
-         {
-           calc.display = 0;
-           updateDisplay(calc.display);
-         }
+        function clearCurrent()
+        {
+          calc.display = 0;
+          updateDisplay(calc.display);
+        }
 
-         function allClear()
-         {
-           calc.display = 0;
-           calc.total = 0;
-           updateDisplay(calc.display);
-         }
+        function allClear()
+        {
+          calc.display = 0;
+          calc.total = 0;
+          updateDisplay(calc.display);
+        }
 
-         function updateDisplay(val)
-      {
-         $display.val(val);
+        function updateDisplay(val)
+        {
+          $display.val(val);
+        }
       }
-       }
-     });
-   });
+    });
+  });
 })();
