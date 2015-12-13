6dead2049661da2834a7a896bd39f260a931298f
jktravis <joshua.travis@gmail.com>
2015-12-13 18:17:37 -0500

    Now using external calc object.

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,25 @@
 (function(){
    $(document).ready(function()
    {
-      var $display = $('.output').first();
-      var total = 0;
-      var display;
+     $.ajax({
+       url: 'static/js/calculator.js',
+       dataType: "script",
+       success: function (data, status, jqxhr)
+       {
+         console.log(exports.calculator.add(1, 2));
+         var $display = $('.output').first();
+         var total = 0;
+         var calcVal = '';
+         var display = 0;
+         var op = false;
 
-      upDateDisplay(total);
+         updateDisplay(total);
 
       $('button').on('click', function()
       {
          var currentButton = $(this).val();
-         if (!isNaN(parseInt(currentButton)))
+        // if the button is a number or a period
+        if (!isNaN(parseInt(currentButton)) || currentButton === '.')
          {
             //store value for math operation
             if (display)
@@ -21,18 +30,62 @@
             {
                display = currentButton;
             }
-            upDateDisplay(display);
+           calcVal = display;
+           updateDisplay(display);
          }
          else
          {
             // store operation to perform function
-            console.log('operation');
+           switch (currentButton)
+           {
+             case 'AC':
+               allClear();
+               break;
+             case 'CE':
+               clearCurrent();
+               break;
+             case '/':
+               console.log('Dividing');
+               break;
+             case '*':
+               console.log('Multiplying');
+               break;
+             case '%':
+               console.log('Percenting');
+               break;
+             case '+':
+               console.log('Adding');
+               break;
+             case '-':
+               console.log('Subtracting');
+               break;
+             case '=':
+               console.log('Equals');
+               break;
+             default:
+               break;
+           }
          }
       });
 
-      function upDateDisplay(val)
+         function clearCurrent()
+         {
+           display = 0;
+           updateDisplay(display);
+         }
+
+         function allClear()
+         {
+           display = 0;
+           total = 0;
+           updateDisplay(display);
+         }
+
+         function updateDisplay(val)
       {
          $display.val(val);
       }
+       }
+     });
    });
 })();
